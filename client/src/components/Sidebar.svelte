b06c4633b4146b7370b4cb2bd681d522223e62cf
หนมจีบ <onopingping@gmail.com>
2024-05-06 19:26:18 +0700

    feat : add side bar page

---
+++ client/src/components/Sidebar.svelte
@@ -0,0 +1,22 @@
+<script lang='ts'>
+    import logo from '../asset/logo.png'
+
+    let scrollY = 0
+    $:{
+        console.log('scorll ' + scrollY)
+    }
+</script>
+
+
+<main class='h-full flex flex-col items-center  '>
+    <img class='w-3/4' src={logo} alt='logo'>
+    <div class="w-3/4 h-fit bg-red-700 flex flex-col items-center">
+        {#each Array(5) as _, i}
+            <div class="w-full h-20 bg-blue-700 flex justify-center items-center hover:text-red-700">
+                <p>Menu {i}</p>
+            </div>
+        {/each}
+    </div>
+</main>
+
+<svelte:window bind:scrollY={scrollY} />
