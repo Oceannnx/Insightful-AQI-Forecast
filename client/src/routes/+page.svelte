209e3762317276f3a580debd629f3bfe6a324b1c
หนมจีบ <onopingping@gmail.com>
2024-05-06 19:26:01 +0700

    feat : design main page structure

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,8 +1,40 @@
-<script>
-  import Chart from '../components/Chart.svelte';
-  export let data
-</script>
+<script lang='ts'>
+    import { getLocation } from '$lib/Geolocation'
+    import imgUrl from '../asset/bangkok_city_2.jpg'
+	import Content from '../components/Content.svelte';
+	import Sidebar from '../components/Sidebar.svelte';
+    export let data
+    
 
+    const handleStarterButton = () => {
+        data.visited = true
+    }
+    getLocation()
+
+
+</script>
 <main>
-  <Chart {data} />
+
+{#if !data.visited}
+<body>
+    <img class='w-screen h-screen -z-10 absolute' src={imgUrl} alt="Bangkok City" />
+    <div class='w-fit h-screen flex flex-col justify-center items-start p-10 bg-slate-400 opacity-80 trapezoid'>
+        <h1 class='text-6xl py-3'>Insightful AQI Forecast</h1>
+        <div class="py-6">
+            <button class='text-xl hover:' on:click={handleStarterButton}>เริ่มต้นใช้งาน</button>
+        </div>
+    </div>
+</body>
+{:else}
+<main class='w-screen h-screen grid grid-cols-8'>
+    <div class="bg-amber-700">
+        <Sidebar />
+    </div>
+    <div class="col-span-7 bg-neutral-300">
+        <Content />
+    </div>
 </main>
+{/if}
+</main>
+
+
