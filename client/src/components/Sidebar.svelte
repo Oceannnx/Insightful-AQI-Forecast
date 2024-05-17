9dc9ca7ee4cdfc06c4015e3bf198401c7d5410a0
หนมจีบ <onopingping@gmail.com>
2024-05-17 18:50:31 +0700

    fix : hover change color

--- client/src/components/Sidebar.svelte
+++ client/src/components/Sidebar.svelte
@@ -12,8 +12,8 @@
     <img class='w-3/4' src={logo} alt='logo'>
     <div class="w-3/4 h-fit bg-red-700 flex flex-col items-center">
         {#each Array(5) as _, i}
-            <div class="w-full h-20 bg-blue-700 flex justify-center items-center hover:text-red-700">
-                <p>Menu {i}</p>
+            <div class="w-full h-20 bg-blue-700 flex justify-center items-center ">
+                <p class="hover:text-red-700">Menu {i}</p>
             </div>
         {/each}
     </div>
