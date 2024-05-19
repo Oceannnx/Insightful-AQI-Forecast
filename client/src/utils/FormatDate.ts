8fce659a9e02cc9e97c89ac8a7f1844c1f7ddd73
หนมจีบ <onopingping@gmail.com>
2024-05-19 14:24:26 +0700

    utils : add format date

---
+++ client/src/utils/FormatDate.ts
@@ -0,0 +1,7 @@
+export const formatDate = (date: string) => {
+    const dayNameTh = ['อาทิตย์', 'จันทร์', 'อังคาร', 'พุธ', 'พฤหัสบดี', 'ศุกร์', 'เสาร์']
+    const monthNameTH = ['มกราคม', 'กุมภาพันธ์', 'มีนาคม', 'เมษายน', 'พฤษภาคม', 'มิถุนายน', 'กรกฎาคม', 'สิงหาคม', 'กันยายน', 'ตุลาคม', 'พฤศจิกายน', 'ธันวาคม']
+    const dateObj = new Date(date)
+
+    return `${dayNameTh[dateObj.getDay()]}, ${dateObj.getDate()} ${monthNameTH[dateObj.getMonth()]}`
+}
