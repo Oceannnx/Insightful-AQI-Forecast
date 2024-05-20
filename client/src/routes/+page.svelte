70f2c76571d873f63e5055e7b0695d993c57adbf
หนมจีบ <onopingping@gmail.com>
2024-05-20 18:35:02 +0700

    fix : remove geolocation

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { getLocation } from '$lib/Geolocation';
 	import imgUrl from '../asset/bangkok_city_2.jpg';
 	import Content from '../components/Content.layout.svelte';
 	import Sidebar from '../components/Sidebar.svelte';
@@ -8,30 +7,46 @@
 	const handleStarterButton = () => {
 		data.visited = true;
 	};
-	getLocation();
 </script>
 
 <main class="overflow-x-hidden">
 	{#if !data.visited}
 		<body>
-			<img class="w-screen h-screen -z-10 absolute" src={imgUrl} alt="Bangkok City" />
-			<div
-				class="w-fit h-screen flex flex-col justify-center items-start p-10 bg-slate-400 opacity-80"
-			>
-				<h1 class="text-6xl py-3">Insightful AQI Forecast</h1>
+			<img class="w-screen h-screen -z-10 absolute blur" src={imgUrl} alt="Bangkok City" />
+			<div class="w-screen h-screen flex flex-col justify-center items-center p-10">
+				<h1 class="text-7xl font-bold py-3 text-background">Insightful AQI Forecast</h1>
 				<div class="py-6">
-					<button class="text-xl hover:" on:click={handleStarterButton}>เริ่มต้นใช้งาน</button>
+					<button
+						class="text-4xl p-3 rounded-2xl bg-white bg-opacity-50 hover:bg-opacity-70"
+						on:click={handleStarterButton}>เริ่มต้นใช้งาน</button
+					>
 				</div>
 			</div>
 		</body>
 	{:else}
-		<main class="w-screen h-screen grid grid-cols-8">
-			<div class="bg-amber-700">
+		<main class="w-screen h-screen grid grid-cols-6">
+			<div class="bg-amber-700 col-span-1">
 				<Sidebar />
 			</div>
-			<div class="col-span-7 bg-neutral-300">
+			<div class="col-span-5 bg-neutral-300">
 				<Content />
 			</div>
 		</main>
 	{/if}
 </main>
+
+<style>
+	.text-background {
+		background-image: url('../asset/bangkok_city_3.jpg');
+		background-repeat: repeat;
+		-webkit-background-clip: text;
+		background-clip: text;
+		-webkit-text-fill-color: transparent;
+		text-align: center;
+		font-weight: bold;
+		text-transform: uppercase;
+		font-family: 'Steelfish Rg', 'helvetica neue', helvetica, arial, sans-serif;
+		font-weight: 800;
+		-webkit-font-smoothing: antialiased;
+	}
+</style>
