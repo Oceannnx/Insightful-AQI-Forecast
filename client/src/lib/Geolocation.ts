c8581d3efb4d47ac0aaaa3d8c577435593642cf6
หนมจีบ <onopingping@gmail.com>
2024-05-20 16:56:30 +0700

    fix : remove geolocation

--- client/src/lib/Geolocation.ts
+++
@@ -1,9 +0,0 @@
-export const getLocation = () => {
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition((position) => {
-			console.log(position.coords.latitude, position.coords.longitude);
-		});
-	} else {
-		console.log('Geolocation is not supported by this browser.');
-	}
-};
