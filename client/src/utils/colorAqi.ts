425bf05881cc103765bdd9815300b78d6eb031d0
หนมจีบ <onopingping@gmail.com>
2024-05-21 15:32:11 +0700

    fix : 100 and 50

--- client/src/utils/colorAqi.ts
+++ client/src/utils/colorAqi.ts
@@ -1,9 +1,9 @@
 export const colorAqi = (aqi:number) => {
     if(aqi >= 0 && aqi <= 50){
         return 'bg-emerald-500 border-emerald-600'
-    } else if(aqi >= 51 && aqi <= 100){
+    } else if(aqi >= 50 && aqi <= 100){
         return 'bg-green-500 border-green-600'
-    } else if(aqi >= 101 && aqi <= 150){
+    } else if(aqi >= 100 && aqi <= 150){
         return 'bg-yellow-500 border-yellow-600'
     } else if(aqi >= 150 && aqi <= 200){
         return 'bg-orange-500 border-orange-600'
