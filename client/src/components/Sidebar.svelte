0d4e7f3a1f3b26e3e144284095c8a223ea105503
หนมจีบ <onopingping@gmail.com>
2024-05-21 15:36:11 +0700

    feat : fixed sidebar

--- client/src/components/Sidebar.svelte
+++ client/src/components/Sidebar.svelte
@@ -4,13 +4,15 @@
 </script>
 
 <main class="flex flex-col items-center bg-fuchsia-800">
-	<img class="w-3/4" src={logo} alt="logo" />
-	<div class="w-3/4 h-fit bg-red-700 flex flex-col items-center">
-		{#each Array(5) as _, i}
-			<div class="w-full h-20 bg-blue-700 flex justify-center items-center">
-				<p class="hover:text-red-700">Menu {i}</p>
-			</div>
-		{/each}
+	<div class="flex flex-col items-center fixed top-0">
+		<img class="w-3/4" src={logo} alt="logo" />
+		<div class="w-3/4 h-fit bg-red-700 flex flex-col items-center">
+			{#each Array(5) as _, i}
+				<div class="w-full h-20 bg-blue-700 flex justify-center items-center">
+					<p class="hover:text-red-700">Menu {i}</p>
+				</div>
+			{/each}
+		</div>
 	</div>
 </main>
 
