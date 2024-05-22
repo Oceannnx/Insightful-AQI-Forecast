4c24404da410e241976ccc9b3381aee1b4ffcb24
หนมจีบ <onopingping@gmail.com>
2024-05-22 13:30:43 +0700

    fix : label

--- client/src/components/ColorLabel.svelte
+++ client/src/components/ColorLabel.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <main class="flex rounded-md bg-neutral-300 drop-shadow-2xl w-3/4 p-10 flex-col">
-	<h1>ระดับของคุณภาพอากาศ และ คำอธิบายสี</h1>
+	<h1 class="text-3xl pb-5">ระดับของคุณภาพอากาศ และ คำอธิบายสี</h1>
 	{#each colorLabel as data}
 		<div class="grid grid-cols-4 p-2 border {data.color}">
 			<div>{data.aqi}</div>
