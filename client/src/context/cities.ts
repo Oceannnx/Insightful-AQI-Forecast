010888c353cf832d91de858562bead0273dbdb42
หนมจีบ <onopingping@gmail.com>
2024-05-22 20:19:49 +0700

    fix : fix wrong spell

--- client/src/context/cities.ts
+++ client/src/context/cities.ts
@@ -1,1 +1,1 @@
-export const cities = [{id:'Bangkok',label:'กรุงเทพ'}, {id:'Chiang-Mai',label:'เชียงใหม่'}, {id:'Nonthaburi',label:'นนทบุรี'}, {id:'samutprakan',label:'สมุทรปราการ'}];
+export const cities = [{id:'Bangkok',label:'กรุงเทพ'}, {id:'Chiang-Mai',label:'เชียงใหม่'}, {id:'Nonthaburi',label:'นนทบุรี'}, {id:'Samutprakan',label:'สมุทรปราการ'}];
