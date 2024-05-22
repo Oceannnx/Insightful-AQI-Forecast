4e10906e6430f0b5addd5215082d755c6557cbb4
หนมจีบ <onopingping@gmail.com>
2024-05-22 22:29:59 +0700

    fix : add center label

--- client/src/context/pageContent.ts
+++ client/src/context/pageContent.ts
@@ -1,7 +1,7 @@
 export const pageContent = [
-    {label:'คุณภาพอากาศวันนี้', startScroll: 0, endScroll: 181.81817626953125},
-    {label:'พยากรณ์คุณภาพอากาศ<br>ล่วงหน้า 7 วัน', startScroll: 181.81817626953125, endScroll: 636.3636474609375}, 
-    {label:'เลือกช่วงเวลาพยากรณ์', startScroll: 636.3636474609375, endScroll:1272.727294921875},
-    {label:'ประวัติคุณภาพอากาศ', startScroll: 1272.727294921875, endScroll:2127.272705078125},
-    {label:'ระดับของคุณภาพอากาศ <br>และคำอธิบายสี', startScroll: 2127.272705078125, endScroll:9999}
+    {label:'คุณภาพอากาศวันนี้', startScroll: 0, endScroll: 181.81817626953125 , center:0},
+    {label:'พยากรณ์คุณภาพอากาศ<br>ล่วงหน้า 7 วัน', startScroll: 181.81817626953125, endScroll: 636.3636474609375, center:260}, 
+    {label:'เลือกช่วงเวลาพยากรณ์', startScroll: 636.3636474609375, endScroll:1272.727294921875, center: 820},
+    {label:'ประวัติคุณภาพอากาศ', startScroll: 1272.727294921875, endScroll:2127.272705078125, center:1480},
+    {label:'ระดับของคุณภาพอากาศ <br>และคำอธิบายสี', startScroll: 2127.272705078125, endScroll:9999, center: 2330}
 ]
