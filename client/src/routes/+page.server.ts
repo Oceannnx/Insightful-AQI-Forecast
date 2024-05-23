8cae8f7f4eff116207d23ff138dadec415ff2e31
หนมจีบ <onopingping@gmail.com>
2024-05-23 16:45:49 +0700

    fix : cookies 10 years

--- client/src/routes/+page.server.ts
+++ client/src/routes/+page.server.ts
@@ -1,9 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const load = ({ cookies }: { cookies: any }) => {
 	const visited = cookies.get('visited');
+	const maxAge = 60 * 60 * 24 * 365 * 10;
 
-	cookies.set('visited', 'true', { path: '/' });
-
+	cookies.set('visited', 'true', 
+	{	path: '/',
+		httpOnly: true, 
+    	secure: true,   
+        maxAge: maxAge
+	})  
 	return {
 		visited
 	};
