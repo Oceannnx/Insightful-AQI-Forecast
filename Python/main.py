a9212a4b4560b21bcbb0c5f1c09915651c7e6aa1
หนมจีบ <onopingping@gmail.com>
2024-05-24 17:32:10 +0700

    fix : add cors route

--- Python/main.py
+++ Python/main.py
@@ -22,7 +22,10 @@ db = client.get_database('InsightfulAqiForecast')
 origins = [
     "http://localhost:5173",
     "http://localhost:8080",
-    "https://insightful-aqi-forecast-8rx92cz0f-oceannnxs-projects.vercel.app/"
+    "https://insightful-aqi-forecast-8rx92cz0f-oceannnxs-projects.vercel.app/",
+    "https://insightful-aqi-forecast-hh3p96m2g-oceannnxs-projects.vercel.app/",
+    "https://insightful-aqi-forecast-git-main-oceannnxs-projects.vercel.app/",
+    "https://insightful-aqi-forecast-oceannnxs-projects.vercel.app/",
 ]
 
 app.add_middleware(
