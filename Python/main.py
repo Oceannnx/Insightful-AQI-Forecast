fb5cdb3958288dcfa7ff58ba6570205c7c35344c
หนมจีบ <onopingping@gmail.com>
2024-05-25 17:06:51 +0700

    fix : cors api route all

--- Python/main.py
+++ Python/main.py
@@ -20,13 +20,13 @@ client = pymongo.MongoClient(MONGODB_URL)
 db = client.get_database('InsightfulAqiForecast')
 
 origins = [
-    "http://localhost",
+    "http://localhost:5173",
     "https://insightful-aqi-forecast.vercel.app/",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
