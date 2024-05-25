7914106104295529b75a3ac80cf992eeac5687b7
หนมจีบ <onopingping@gmail.com>
2024-05-25 17:18:50 +0700

    fix : cors api route, add regex

--- Python/main.py
+++ Python/main.py
@@ -30,6 +30,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    allow_origin_regex='https://insightful-aqi-forecast.vercel.app/'
 )
 
 def now():
