fa953b2ad712723da6851cfb1ee05294e40f10ca
หนมจีบ <onopingping@gmail.com>
2024-05-25 17:57:24 +0700

    fix: add header axioslib

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -2,5 +2,8 @@ import axios from 'axios';
 
 export const AxiosLib = axios.create({
 	baseURL: import.meta.env.VITE_BACKEND_URL ?? 'https://insightful-aqi-forecast.onrender.com',
-	withCredentials: true
+	withCredentials: true,
+	headers: {
+		'Content-Type': 'application/json',
+	},
 });
