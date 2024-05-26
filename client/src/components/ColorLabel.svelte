213b8d11c3ce3b9117612a1e00aee0eafad30cda
หนมจีบ <onopingping@gmail.com>
2024-05-26 14:27:24 +0700

    fix : add header label

--- client/src/components/ColorLabel.svelte
+++ client/src/components/ColorLabel.svelte
@@ -4,6 +4,12 @@
 
 <main class="flex rounded-md bg-neutral-300 drop-shadow-2xl w-3/4 p-10 flex-col">
 	<h1 class="text-3xl pb-5">ระดับของคุณภาพอากาศ และ คำอธิบายสี</h1>
+	<div class="grid grid-cols-4 p-2 border bg-neutral-200">
+		<div>ค่า AQI</div>
+		<div>ระดับ</div>
+		<div>ผลกระทบต่อสุขภาพ</div>
+		<div>คำเตือน</div>
+	</div>
 	{#each colorLabel as data}
 		<div class="grid grid-cols-4 p-2 border {data.color}">
 			<div>{data.aqi}</div>
